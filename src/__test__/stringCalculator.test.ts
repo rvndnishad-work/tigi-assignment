6058262c97c2ab98f4f9bbc3e70405cbc180b02d
arvindnishad <rvndnishad@gmail.com>
2024-08-07 05:02:18 +0530

    added tests for positive scenario for string additions

--- src/__test__/stringCalculator.test.ts
+++ src/__test__/stringCalculator.test.ts
@@ -12,4 +12,16 @@ describe('String Calculator', () => {
     test('should throw an error for multiple negative numbers', () => {
         expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
     });
+
+    test('should return the number itself for a single number', () => {
+        expect(add('1')).toBe(1);
+    });
+
+    test('should return the sum for two numbers', () => {
+        expect(add('1,5')).toBe(6);
+    });
+
+    test('should return the sum for multiple numbers', () => {
+        expect(add('1,2,3')).toBe(6);
+    });
 });
